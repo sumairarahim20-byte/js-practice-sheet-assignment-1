c9b8e0db66587b866113fb20c101da848d4be5b5
sumairarahim20-byte <sumairarahim20@gmail.com>
2026-02-17 23:14:16 +0500

    chapter-01-pdf-complete

---
+++ index.js
@@ -0,0 +1,82 @@
+////// CHAPTER # 01 //////// 
+
+// Q-01//
+alert("welcome to my website!")
+
+
+// Q-02//
+let password = prompt ("Enter your password");
+
+if(password !=="abcd"){
+    alert("Error! please enter a valid password")
+}else{
+    alert("welcome to our page")
+}
+
+
+// Q-03//
+
+alert(`welcome to js land...
+    Happy coding`)
+
+
+    // Q-04//
+
+
+    alert("welcome to js land...");
+
+
+    alert("happy coding!\n\
+        prevent this page from creating additional dialogs.");
+
+
+
+        // Q-05//
+
+
+        alert("Hello... I can run js through my web browser's console")
+
+
+
+        // Q-07//
+
+
+// <head>
+    // <meta charset="UTF-8">
+    /* <meta name="viewport" content="width=device-width, initial-scale=1.0"> */
+    /* <title>Document</title> */
+
+    /* <script> */
+        /* console.log("script inside the head section") */
+    /* </script> */
+/* </head> */
+
+       
+/* <script>
+        console.log("script in body,befor HTML content")
+    </script> */
+
+
+//      <div>
+//     <script>
+//         console.log("script inside the page's HTML(between elements)");
+//     </script>
+// <p>script inside the page's HTML between elements</p>
+// </div>
+
+
+
+/* <script>
+    alert("script in body ,after all HTML content");
+    console.log("script in body ,after all HTML content")
+</script> */
+
+
+
+// ////////////////////////////////////////////////////////////////
+
+
+
+// /////// CHAPTER # 02 ////////
+
+
